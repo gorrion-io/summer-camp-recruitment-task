9b51f86dcf7caac43f6e486def9bdbe74113404b
Błażej Hanzel <blazej.hanzel@gmail.com>
2022-04-21 10:00:14 +0200

    getting users from json file

--- lib/users.ts
+++ lib/users.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import { readFileSync } from 'fs';
+import path from "path";
 
 const userSchema = z.object({
   fullName: z.string().min(2).max(50),
@@ -23,5 +25,33 @@ const userSchema = z.object({
 type User = z.infer<typeof userSchema>;
 
 export async function getAllUsers(): Promise<User[]> {
-  return [];
+  const json_file = readFileSync(path.resolve(process.cwd(), 'users.json'), 'utf-8');
+  const csv_file = readFileSync(path.resolve(process.cwd(), 'users.csv'), 'utf-8');
+
+  let users: User[] = [];
+
+  for (const item of JSON.parse(json_file)) {
+    const itemNewBind = {
+      fullName: item.full_name,
+      username: item.nickname,
+      email: item.email_address,
+      avatar: item.user_image,
+      address: {
+        street: item.user_address.street_address,
+        city: item.user_address.city.city_name,
+        zip: item.user_address.city.city_zip_code,
+      },
+      phoneNumber: item.phone_number,
+      gender: item.gender,
+      age: item.age,
+      images: item.imgs,
+    }
+
+    const user = userSchema.safeParse(itemNewBind);
+    if (user.success) {
+      users.push(user.data);
+    }
+  }
+
+  return users;
 }
--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,8 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getAllUsers } from "../../lib/users";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  res.status(501).json({ message: "Not implemented" });
+  res.status(200).json(await getAllUsers());
 }
