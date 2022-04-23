525f6871aba1d539fe29a12ced7589f861d14bcf
Mateusz Penkala <mateuszpenkala@gmail.com>
2022-04-23 22:48:42 +0200

    Add user cards display in users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,11 +1,36 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import styles from '../styles/pages/Users.module.scss';
 import Layout from '../components/Layout/Layout';
+import UserCard from '../components/UserCard/UserCard';
+import { getAllUsers, User } from '../lib/users';
 
-const Users: NextPage = () => {
+export async function getServerSideProps() {
+  const users = await getAllUsers();
+
+  return {
+    props: { users },
+  };
+}
+
+interface Props {
+  users: User[];
+}
+
+const Users: NextPage<Props> = ({ users }) => {
   return (
     <Layout>
-      <main className={styles.main}></main>
+      <Head>
+        <title>Gorrion Summer Camp 2022 - recruitment task</title>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Users</h1>
+        <div className={styles.userCards}>
+          {users && users.map((user) => <UserCard user={user} key={user.email} />)}
+        </div>
+      </main>
     </Layout>
   );
 };
