fad30e20030ea244a784f832b585900bafa28ea8
Radosław Rzepka <radek.rz.123@gmail.com>
2022-04-25 18:44:40 +0200

    added ImageViewer

--- src/components/UserImages/UserImages.tsx
+++ src/components/UserImages/UserImages.tsx
@@ -1,16 +1,26 @@
 import styles from "./UserImages.module.css";
-import React from "react";
+import React, { useState } from "react";
+import ImageViewer from "./ImageViewer";
 
 const UserImages = (props: any) => {
 	const userImages = props.userImages;
 
+	const [showComponent, setShowComponent] = useState(false);
+
+	const showImageViewer = () => {
+		setShowComponent(true);
+	};
+
 	return (
 		<div className={styles["images-buttons"]}>
-			{userImages.map((image: string, index: number) => (
-				<button style={{ width: `${100 / userImages.length}%` }} key={index} className={styles["images-button"]}>
-					Image {index + 1}
-				</button>
-			))}
+			{userImages.map((image: string, index: number) => {
+				return (
+					<button style={{ width: `${100 / userImages.length}%` }} key={index} className={styles["images-button"]} onClick={showImageViewer}>
+						Image {index + 1}
+					</button>
+				);
+			})}
+			{showComponent ? <ImageViewer src={userImages[0]} isActive={setShowComponent} /> : null}
 		</div>
 	);
 };
