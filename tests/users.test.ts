d2133212f6d5e0a646eb1d97a5d4593d346ff09f
Wojciech <szmelczerczykwojciech@gmail.com>
2022-05-01 18:58:16 +0200

    test description improved

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -2,7 +2,7 @@ import getAllUsers from "../lib/users";
 import { User } from "../types/userTypes";
 
 describe("Check users", () => {
-  test("check if there is user which doesn't meet age requirements (18-65)", async () => {
+  test("check if all users meet age requirements (18-65)", async () => {
     const users = await getAllUsers();
     users.forEach((user: User) => {
       expect(user.age).toBeGreaterThanOrEqual(18);
