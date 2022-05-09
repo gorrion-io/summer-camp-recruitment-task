aa44ba8b7f7d83d76c19f5d7eda6b249694c2e1a
Mathias007 <mathias.najlepszy@gmail.com>
2022-05-09 22:07:05 +0200

    install and implement Sass. Remove css files. Create base.scss, variables.scss and main.scss

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
+    return {
+        ...defaultConfig,
 
-    webpack: (config) => {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          "fs": false,
-          "path": false,
-          "os": false,
-        }
-      }
-      return config
-    },
-  }
-}
+        images: {
+            domains: ["cloudflare-ipfs.com"],
+        },
+
+        webpack: (config) => {
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    fs: false,
+                    path: false,
+                    os: false,
+                },
+            };
+            return config;
+        },
+    };
+};
