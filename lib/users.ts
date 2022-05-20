2e2f7dc3eb9c38838624bfc1d61834ea88b0fb71
gamedevturdus <kubaluk757@o2.pl>
2022-05-20 13:20:50 +0200

    csv data gets converted to json

--- lib/users.ts
+++ lib/users.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import csv from "csvtojson";
 
 const userSchema = z.object({
   fullName: z.string().min(2).max(50),
@@ -23,5 +24,34 @@ const userSchema = z.object({
 type User = z.infer<typeof userSchema>;
 
 export async function getAllUsers(): Promise<User[]> {
+  const csvParserConfig = {
+    delimiter: ",",
+    trim: true,
+    includeColumns: /(name|email|avatar|address.street|address.city|address.zipcode|phone|bio|images)/,
+    colParser: {
+      "bio.dob":function(item: string, head: string){
+        return getAge(item)
+      }
+    }
+  }
+
+  await fetch('users.csv')
+    .then(response => response.text())
+    .then(data => csv(csvParserConfig).fromString(data))
+    .then((parsed)=>console.log(parsed))
+    .catch((error) => console.log(error))
+  
+  function getAge(dateOfBirth: string){
+    var today = new Date();
+    var birthDate = new Date(dateOfBirth);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
+
   return [];
 }
--- pages/users.tsx
+++ pages/users.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
+import { getAllUsers } from "../lib/users";
 
 const Users: NextPage = () => {
+  getAllUsers()
   return <h1>Not implemented</h1>;
 };
 
